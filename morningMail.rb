994bf025c16ffcf225b69fb8020a3c2f5658d318
Jeff Sacks <jrsacks@gmail.com>
2014-01-02 12:14:35 -0600

    adding tennis scores

--- morningMail.rb
+++ morningMail.rb
@@ -58,8 +58,36 @@ def print_sport(sport, team, add_date=true)
   end
 end
 
+def print_tennis
+  yesterday = (Date.today - 1).strftime "%Y%m%d" 
+  url = "http://espn.go.com/tennis/dailyResults?date=#{yesterday}&matchType=msingles"
+  doc = Nokogiri::HTML(open(url))
+  last_event_title = ''
+  doc.css('.matchContainer').each do |m|
+    event = m.parent
+    until event.attr('class') == 'scoreHeadline'
+      event = event.previous
+    end
+    event_title = event.css('a').text 
+    puts event_title unless event_title == last_event_title
+    last_event_title = event_title
+
+    first_winner = '*'
+    second_winner = "" 
+    if m.css('.arrow').attr('style').value.match /43/
+      second_winner = "*" 
+      first_winner = ''
+    end
+
+    printf "%-1s%-25s %s\n", first_winner, m.css('.teamLine a').text, (m.css('.lsLine2').map { |s| s.text }).join(' ')
+    printf "%-1s%-25s %s\n", second_winner, m.css('.teamLine2 a').text, (m.css('.lsLine3').map { |s| s.text }).join(' ')
+    empty_line
+  end
+end
+
 print_weather
 print_sport('college-basketball', 'michigan-wolverines')
 print_sport('nfl', 'chicago-bears', false)
 print_sport('nba', 'chicago-bulls')
 print_sport('nhl', 'chicago-blackhawks')
+print_tennis
